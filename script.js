bc6eb30217895043e8f2837e3ed3b6cd74d04fa3
List Ganga <42545091+listganga@users.noreply.github.com>
2020-11-10 00:53:57 +0530

    version 1.1

---
+++ script.js
@@ -0,0 +1,34 @@
+var total = 6;
+function createSlick(){  
+
+	$(".slider").not('.slick-initialized').slick({
+		centerMode: true,
+	    autoplay: true,
+	    dots: true,
+	
+  		slidesToShow: 3,
+	    responsive: [{ 
+	        breakpoint: 768,
+	        settings: {
+	            dots: false,
+	            arrows: false,
+	            infinite: false,
+	            slidesToShow: 1,
+	            slidesToScroll: 1
+	        } 
+	    }]
+	});	
+
+}
+
+createSlick();
+
+//Will not throw error, even if called multiple times.
+$(window).on( 'resize', createSlick );
+
+$(document).ready(function(){
+    
+        for(var i = 1 ; i <= total; i++){
+            $(".row1").append('<div class="ilb-m box4 imgwrap"><a class="imgdiv ilb-m" href="#"><img class="box1 ilb-t" src="./images/'+ i +'a.jpg" alt="Legging"><img class="box2a ilb-t" src="./images/'+ i+'b.jpg" alt="Legging"><img class="box2a ilb-t" src="./images/'+ i+'c.jpg" alt="Legging"><p>Visit Store</p></a></div>');
+        };
+});
